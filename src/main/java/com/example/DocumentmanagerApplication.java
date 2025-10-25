7c00395bc31de36a79a3f544da1e1a0b93b31d66
Ashok-6 <ashokaksy123@gmail.com>
2025-10-25 18:35:51 +0530

    new

--- src/main/java/com/example/DocumentmanagerApplication.java
+++ src/main/java/com/example/DocumentmanagerApplication.java
@@ -19,7 +19,7 @@ public class DocumentmanagerApplication {
 	        @Override
 	        public void addCorsMappings(CorsRegistry registry) {
 	            registry.addMapping("/**")
-	                    .allowedOrigins("https://webrepo1vercel.vercel.app")
+	                    .allowedOrigins("https://webrepo12.vercel.app/")
 	                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 	                    .allowedHeaders("*");
 	        }
